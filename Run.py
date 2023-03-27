7a50907821d8faa2eefd29dc1fa1719a2d99ec5e
Mursalin Ahamed Khan Alin <102971155+MAK-GANG@users.noreply.github.com>
2023-03-27 23:36:49 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -6,6 +6,9 @@ if machine()=='aarch64':
 else:
 	time.sleep(2)
 	print("  		\033[1;91m Sorry Bro System Not Supported   \n 		 Contact Me To Use The Tools ")
+	print(" Follow Team Dccs-team On Github ")
+	os.system('xdg-open https://github.com/Dccs-team/ ')
+	time.sleep(3)
 	print("		 Tools Is Free Don’t Worry ")
 	time.sleep(1)
 	os.system('xdg-open https://wa.me/+8801870830179')
